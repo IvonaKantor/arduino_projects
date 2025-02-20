b119e64799cfe84e612702b411e064e335ff2dc3
ivona <ik87056@student.uwb.edu.pl>
2025-02-20 14:01:22 +0100

    reading status of buttons

--- automatic_light/automatic_light.ino
+++ automatic_light/automatic_light.ino
@@ -1,7 +1,7 @@
-#define button1 4;
-#define button2 5;
-#define led1 2;
-#define led2 3;
+#define button1 4
+#define button2 5
+#define led1 2
+#define led2 3
 
 void setup() {
   pinMode(button1, INPUT_PULLUP);
@@ -11,6 +11,13 @@ void setup() {
 }
 
 void loop() {
-  int level1 = degitalRead(button1);
+  int level1 = digitalRead(button1);
   int level2 = digitalRead(button2);
+
+  if(level1 == LOW){
+    digitalWrite(led1, HIGH);
+  }
+  else if(level2 == LOW){
+    digitalWrite(led2, HIGH);
+  }
 }
