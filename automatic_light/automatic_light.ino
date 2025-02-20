896821630dd206b9d63ea9515b2a1748e6d2b50d
ivona <ik87056@student.uwb.edu.pl>
2025-02-20 17:46:49 +0100

    updating of new function

--- automatic_light/automatic_light.ino
+++ automatic_light/automatic_light.ino
@@ -1,10 +1,10 @@
-struct Button{
+struct Button {
   int pin;
   int pressCount;
   bool lastState;
 };
 
-enum State{
+enum State {
   OFF = LOW,
   ON = HIGH
 };
@@ -14,8 +14,8 @@ enum State{
 #define BUTTON1_PIN 4
 #define BUTTON2_PIN 5
 
-Button button1 = {BUTTON1_PIN, 0, HIGH};
-Button button2 = {BUTTON2_PIN, 0, HIGH};
+Button button1 = { BUTTON1_PIN, 0, HIGH };
+Button button2 = { BUTTON2_PIN, 0, HIGH };
 
 void setup() {
   Serial.begin(9600);
@@ -27,35 +27,23 @@ void setup() {
 }
 
 void loop() {
- function(button1);
- function(button2);
+  function(button1, LED1_PIN);
+  function(button2, LED2_PIN);
 }
 
-void function(Button &button){
- 
+void function(Button &button, int ledPin) {
+
   int level = digitalRead(button.pin);
 
-  if (level1 == LOW && state1 == HIGH) {
-    pressCount1++;
-    Serial.println("Pressed button1");
+  if (level == LOW && button.lastState == HIGH) {
+    button.pressCount++;
+    Serial.println("Pressed a button");
   }
 
-  if (level1 == HIGH && state1 == LOW && pressCount1 == 2) {
-    digitalWrite(led1, HIGH);
+  if (level == HIGH && button.lastState == LOW && button.pressCount == 2) {
+    digitalWrite(ledPin, ON);
     Serial.println("Level1 low");
-    pressCount1 = 0;
-  } 
-  state1 = level1;
-
-  if (level2 == LOW && state2 == HIGH) {
-    pressCount2++;
-    Serial.println("Pressed button2");
+    button.pressCount = 0;
   }
-
-  if (level2 == HIGH && state2 == LOW && pressCount2 == 2) {
-    digitalWrite(led2, HIGH);
-    Serial.println("Level2 low");
-    pressCount2 = 0;
-  }
-  state2 = level2;
+  button.lastState = level;
 }
