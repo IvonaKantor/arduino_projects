d0910a1a384af2723f1e7b63e6f05f1e2e7a15f0
ivona <ik87056@student.uwb.edu.pl>
2025-02-20 13:49:55 +0100

    added pin and resistors

--- automatic_light/automatic_light.ino
+++ automatic_light/automatic_light.ino
@@ -1,5 +1,13 @@
+#define button1 4;
+#define button2 5;
+#define led1 2;
+#define led2 3;
 
 void setup() {
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
 void loop() {
